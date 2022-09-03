9dcf0668650ca8ca892d6e32fabaeb4f02472a44
root <root@ip-172-31-22-13.eu-west-1.compute.internal>
2022-09-03 07:12:59 +0000

    ...

--- run.sh
+++ run.sh
@@ -19,8 +19,4 @@ echo "Validatin network yaml"
 ajv validate -s /home/bevel/platforms/network-schema.json -d /home/bevel/build/network.yaml 
 
 echo "Running the playbook..."
-<<<<<<< HEAD
-exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -vvvvv
-=======
 exec ansible-playbook -vv /home/bevel/platforms/shared/configuration/site.yaml --inventory-file=/home/bevel/platforms/shared/inventory/ -e "@/home/bevel/build/network.yaml" -e 'ansible_python_interpreter=/usr/bin/python3' -vvv
->>>>>>> 40d20933d6d70553df7a7dcc7ea772af6125b6cc
